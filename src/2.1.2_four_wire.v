d2c9efdf94ed8c656907979886b733cab2d51311
Giftwen <wenjiabao0919@163.com>
2022-08-26 22:27:28 +0800

    delete some traish

--- src/2.1.2_four_wire.v
+++
@@ -1,8 +0,0 @@
-module top_module( 
-    input a,b,c,
-    output w,x,y,z 
-    );
-        assign {w,x,y,z} = {a,b,b,c} ;
-        
-
-endmodule
--- src/2.3.1Module.v
+++
@@ -1,7 +0,0 @@
-module top_module ( input a, input b, output out );
- mod_a m0(
-     .in1(a),
-     .in2(b),
-     .out(out)
- );
-endmodule
